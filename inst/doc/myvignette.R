2e5670522cdf7b0000400b036665d09577a8c4fc
Matthias Speidel <matthias.speidel@googlemail.com>
2019-11-16 12:30:03 +0000

    version 0.9.17

--- inst/doc/myvignette.R
+++ inst/doc/myvignette.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## $Plant
 ## [1] "ordered_categorical"
 ## 
@@ -14,14 +14,14 @@
 ## $uptake
 ## [1] "cont"
 
-## ----echo = TRUE---------------------------------------------------------
+## ----echo = TRUE--------------------------------------------------------------
 library("lme4")
 ex <- sleepstudy
 set.seed(1)
 ex[sample(1:nrow(CO2), size = 20), "Reaction"] <- NA
 head(ex) # e.g. in line 5 there is a NA now.
 
-## ----eval = TRUE, message = TRUE, cache = TRUE---------------------------
+## ----eval = TRUE, message = TRUE, cache = TRUE--------------------------------
 library("lme4")
 lmer(formula = Reaction~1+Days+(1+Days|Subject), data = ex, na.action = na.omit)
 
@@ -30,6 +30,6 @@ library("hmi")
 set.seed(1)
 result_multi <- hmi(data = ex, model_formula = Reaction ~ 1 + Days + (1 + Days | Subject), maxit = 1, nitt = 1200, burnin = 200)
 
-## ----eval = TRUE, echo = TRUE, message = FALSE, cache = TRUE-------------
+## ----eval = TRUE, echo = TRUE, message = FALSE, cache = TRUE------------------
 result_multi$pooling
 
